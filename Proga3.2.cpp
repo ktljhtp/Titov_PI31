a86d095db4991449defdb96a752d6383fce93d29
ktljhtp <ktljhtp@gmail.com>
2024-09-28 15:56:09 +0700

    добавлены структуры device и playlistsettings

--- Proga3.2.cpp
+++ Proga3.2.cpp
@@ -28,6 +28,31 @@ struct Effect {
     float intensity;      // Интенсивность эффекта
 };
 
+struct AudioSettings {
+    int volume;              // Уровень громкости (0 - 100)
+    int bassLevel;           // Уровень басов (-10 до +10)
+    int trebleLevel;         // Уровень высоких частот (-10 до +10)
+    int balance;             // Баланс между левым и правым каналом (-50 до +50)
+};
+
+struct User {
+    char username[50];          // Имя пользователя
+    int preferredVolume;        // Предпочтительный уровень громкости
+    char preferredCodec[10];    // Предпочтительный аудиоформат
+};
+
+struct Device {
+    char deviceName[100];   // Название устройства
+    int maxVolume;          // Максимальная громкость устройства
+    int currentVolume;      // Текущая громкость устройства
+};
+
+struct PlaylistSettings {
+    int shuffle;          // Включен ли режим случайного воспроизведения (1 — да, 0 — нет)
+    int repeat;           // Включен ли режим повтора (1 — да, 0 — нет)
+    char playlistName[50]; // Название текущего плейлиста
+};
+
 int main()
 {
 
